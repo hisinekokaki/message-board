11e5fb1ae29df6c69437c65c90cf5ca4cc192f95
hisinekokaki <hisineko96@icloud.com>
2015-12-09 19:00:02 +0000

    age

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     #内容は必須入力かつ２文字以上30文字以下
     validates :body , length: {minimum: 2, maximum: 30 } , presence: true
-    
+    #年齢は０以上の数字
+    validates :age , numericality: {greater_than: 1, less_than:120 } 
 end
