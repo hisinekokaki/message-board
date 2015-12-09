126be8a4d71ac0c52fdc60d62376cf8787ae6ed0
hisinekokaki <hisineko96@icloud.com>
2015-12-09 13:19:41 +0000

    8.7

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,24 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def edit
   end
   
-  def update
-    if @message.update(message_params)
-      #保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
-    else
-      #保存に失敗した場合は編集画面へ戻す
-      render 'edit'
-    end
+def update
+  if @message.update(message_params)
+    #保存に成功した場合はトップページへリダイレクト
+    redirect_to root_path , notice: 'メッセージを編集しました'
+  else
+    #保存に失敗した場合は編集画面へ戻す
+    render 'edit'
   end
-  
-  def index
-    # Messageを全て取得する
-    @messages = Message.all
-    @message = Message.new
-  end
+end
+
+def index
+  # Messageを全て取得する
+  @messages = Message.all
+  @message = Message.new
+end
 
 def create
     @message = Message.new(message_params)
@@ -32,8 +32,13 @@ def create
     end
 end
 
+def  destroy
+  @message.destroy
+  redirect_to root_path, notice: 'メッセージを削除しました'
+end
+
   
-  private
+private
   
   def message_params
     params.require(:message).permit(:name, :body)
